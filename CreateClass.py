54c767c5da74f542fcc95e361d229c05b4eceb54
MatthewZellmer <mzellm1@wgu.edu>
2024-10-13 17:03:01 -0500

    Added "a" to end of class name so it did not interfere when running all code in main.py

--- CreateClass.py
+++ CreateClass.py
@@ -1,4 +1,4 @@
-class WeatherLoc:
+class WeatherLoca:
     def __init__(self, latitude, longitude, month, day, year,
                  avg_temp, min_temp, max_temp, avg_wind_speed,
                  min_wind_speed, max_wind_speed, sum_precipitation,
